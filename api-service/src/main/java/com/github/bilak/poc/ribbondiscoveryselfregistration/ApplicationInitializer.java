6e4efd10712de402a9dda7148eb4d9b2c89caf09
bilak <bilaak@gmail.com>
2016-09-27 11:09:05 +0200

    - created EurekaListener as InitializerListener

--- api-service/src/main/java/com/github/bilak/poc/ribbondiscoveryselfregistration/ApplicationInitializer.java
+++ api-service/src/main/java/com/github/bilak/poc/ribbondiscoveryselfregistration/ApplicationInitializer.java
@@ -1,5 +1,10 @@
 package com.github.bilak.poc.ribbondiscoveryselfregistration;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.EurekaEvent;
+import com.netflix.discovery.EurekaEventListener;
+import com.netflix.discovery.StatusChangeEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,13 +26,39 @@ public class ApplicationInitializer implements ApplicationListener<InstanceRegis
 	private RestOperations restTemplate;
 
 	@Autowired
+	private EurekaClient discoveryClient;
+
+	@Autowired
 	public void setRestTemplate(@LoadBalanced RestOperations restTemplate) {
 		this.restTemplate = restTemplate;
 	}
 
 	@Override
-	public void onApplicationEvent(InstanceRegisteredEvent<?> applicationReadyEvent) {
-		ResponseEntity<String> helloResponse = restTemplate.getForEntity("http://api-service/hello-controller/{name}", String.class, "my friend");
-		logger.debug("Response from controller is {}", helloResponse.getBody());
+	public void onApplicationEvent(InstanceRegisteredEvent<?> event) {
+		EurekaEventListener initializerListener = new InitializerListener(discoveryClient, restTemplate);
+		discoveryClient.registerEventListener(initializerListener);
 	}
+
+	public static class InitializerListener implements EurekaEventListener {
+
+		private EurekaClient eurekaClient;
+		private RestOperations restTemplate;
+
+		public InitializerListener(EurekaClient eurekaClient, RestOperations restTemplate) {
+			this.eurekaClient = eurekaClient;
+			this.restTemplate = restTemplate;
+		}
+
+		@Override
+		public void onEvent(EurekaEvent event) {
+			if (event instanceof StatusChangeEvent) {
+				if (((StatusChangeEvent) event).getStatus().equals(InstanceInfo.InstanceStatus.UP)) {
+					ResponseEntity<String> helloResponse = restTemplate.getForEntity("http://api-service/hello-controller/{name}", String.class, "my friend");
+					logger.debug("Response from controller is {}", helloResponse.getBody());
+					eurekaClient.unregisterEventListener(this);
+				}
+			}
+		}
+	}
+
 }
